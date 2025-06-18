2a40c645f456faeb2f286bf073aa115a9902f61f
Harsh1456 <133345911+Harsh1456@users.noreply.github.com>
2025-06-18 10:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1342,8 +1342,11 @@ def generate_pdf_report(project):
     font_config = FontConfiguration()
     return HTML(string=html_content).write_pdf(font_config=font_config)
 
+with app.app_context():
+    db.create_all()
 
 
 # Main: Run the Flask app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
